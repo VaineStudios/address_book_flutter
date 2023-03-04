d9b8459e8bee86f7cdaceb3274e727d816ed36cc
aldaine_dell <vainedev@gmail.com>
2023-03-04 15:22:25 -0500

    created utility file for color palette

---
+++ lib/src/utilities/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  
+  static const gradient1 = Color(0xFF7b4397);
+  static const gradient2 = Color(0xFFDC2430);
+  static const primary = Color(0xFF2F4858);
+  static const secondary = Color(0xFF354E77);
+}
