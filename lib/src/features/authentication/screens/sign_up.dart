a9de78b04d2dd22d07778798bf717728bf3b4ee6
aldaine_dell <vainedev@gmail.com>
2023-03-05 11:05:17 -0500

    created signup screen

--- lib/src/features/authentication/screens/sign_up.dart
+++ lib/src/features/authentication/screens/sign_up.dart
@@ -1,3 +1,5 @@
+import 'package:address_book/src/shared/shared.dart';
+import 'package:address_book/src/utilities/palette.dart';
 import 'package:flutter/material.dart';
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -7,13 +9,170 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+
+
+
+  //Global key for FormState
+  final _formKey = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // resizeToAvoidBottomInset: false,
       body: Stack(
-        children: [
-          Positioned.fill(child: Image(image: AssetImage("assets/images/image_container.png")))
+        children:   [
+          Positioned(top:0 ,bottom: MediaQuery.of(context).size.height * .5, child: const Image(image: AssetImage("assets/images/image_container.png"),alignment: Alignment.bottomRight, fit: BoxFit.fitWidth, )),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 5, blurStyle: BlurStyle.normal, spreadRadius: 1),],
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40))
+              ),
+
+              child: ListView(
+                shrinkWrap: true,
+                keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+                children: [
+                  Center(child: Text("SignUp Page", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30))),
+                  SizedBox(height: 30),
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          // Form Fields
+                          Column(
+                            children: [
+                              Row(
+                                children:  [
+                                  Expanded(child: TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+
+                                    maxLines: 1,
+                                    decoration: const InputDecoration(
+                                        prefixIcon: Icon(Icons.person),
+                                        label: Text("First Name")
+                                    ),
+                                  )),
+                                  SizedBox(width: 20.0,),
+                                  Expanded(child: TextFormField(
+
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+
+                                    maxLines: 1,
+                                    decoration: const InputDecoration(
+                                        prefixIcon: Icon(Icons.person),
+                                        label: Text("Last Name")
+                                    ),
+                                  ),)
+                                ],
+                              ),
+                              TextFormField(
+
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+
+
+                                maxLines: 1,
+                                decoration: const InputDecoration(
+                                    prefixIcon: Icon(Icons.email),
+                                    label: Text("Email")
+                                ),
+                              ),
+                              TextFormField(
+
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+
+                                maxLines: 1,
+                                decoration: const InputDecoration(
+                                    prefixIcon: Icon(Icons.phone),
+                                    label: Text("Mobile Number")
+                                ),
+                              ),
+                              TextFormField(
+
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+
+                                maxLines: 1,
+                                decoration: const InputDecoration(
+                                    prefixIcon: Icon(Icons.lock),
+                                    label: Text("Password")
+                                ),
+                              ),
+                              TextFormField(
+
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+
+                                maxLines: 1,
+                                decoration: const InputDecoration(
+                                    prefixIcon: Icon(Icons.lock_outline),
+                                    label: Text("Confirm Password")
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height:20),
+                          RoundedButtonWidget(buttonText: "Sign Up", width: double.infinity, onpressed: (){}),
+                          SizedBox(height:20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Already have an account? "),
+                              GestureDetector(
+                                  child: Text("Sign In", style:TextStyle(color: Palette.gradient2, fontWeight:FontWeight.bold)),
+                                  onTap: (){},
+                                  ),
+                            ],
+                          )
+                        ],
+                      )
+                  ),
+                ],
+              )
+
+            ),
+          )
         ],
       ),
     );
