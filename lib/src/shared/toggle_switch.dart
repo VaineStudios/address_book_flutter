8d011e5e5749b480302be6978ba08fc74ac289e8
aldaine_dell <vainedev@gmail.com>
2023-03-11 20:16:58 -0500

    made toggle switch more dynamic by making values parameters

--- lib/src/shared/toggle_switch.dart
+++ lib/src/shared/toggle_switch.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 
 class ToggleSwitch extends StatefulWidget{
-  bool toggleState;
-  ToggleSwitch({Key? key, this.toggleState = false}): super(key: key);
+  bool isActive;
+  final Color activeColor;
+  final Color defaultColor;
+  final double height;
+  final double width;
+
+  ToggleSwitch({
+    Key? key,
+    this.isActive = false,
+    this.activeColor = Colors.green,
+    this.defaultColor = Colors.grey,
+    this.height = 27,
+    this.width = 55
+  }): super(key: key);
 
   @override
   State<ToggleSwitch> createState() => _ToggleSwitch();
@@ -19,14 +31,14 @@ class _ToggleSwitch extends State<ToggleSwitch>{
     return GestureDetector(
       onTap:(){
         setState(() {
-          widget.toggleState = !widget.toggleState;
+          widget.isActive = !widget.isActive;
         });
       },
       child: AnimatedContainer(
-        constraints: const BoxConstraints.expand(width:55, height:27),
-        padding: const EdgeInsets.all(2),
+        constraints: BoxConstraints.expand(width:widget.width, height:widget.height),
+        padding: const EdgeInsets.all(1),
         decoration: BoxDecoration(
-          color: widget.toggleState? Colors.green : Colors.grey,
+          color: widget.isActive? widget.activeColor : widget.defaultColor,
           border: Border.all(width: 1, color: Colors.black26),
           borderRadius: const BorderRadius.all(Radius.circular(50)),
             boxShadow:  const [BoxShadow(color:Colors.black87, blurRadius:10 , spreadRadius: -12)]
@@ -34,11 +46,11 @@ class _ToggleSwitch extends State<ToggleSwitch>{
         ),
           duration: const Duration(milliseconds: 300),
           child: AnimatedAlign(
-            alignment: widget.toggleState ?  Alignment.centerRight: Alignment.centerLeft,
+            alignment: widget.isActive ?  Alignment.centerRight: Alignment.centerLeft,
             duration: const Duration(milliseconds: 300),
             child: Container(
-              width:25,
-              height:25,
+              width:widget.height-5, // set the width and height to be the same to ensure that the shape created will be a circle.
+              height:widget.height-5,
               decoration: const BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(50),),
                 boxShadow:  [BoxShadow(color:Colors.black26, blurRadius: 4)]
